2e1495cafc8476864340744fb43156542c6a1fe3
Gerald2008kot <geraldandreacunacorro@gmail.com>
2026-04-15 17:16:13 -0500

    Eliminar pwa.js

--- js/pwa.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('../sw.js')
-      .then(reg => console.log('SW registrado'))
-      .catch(err => console.log('SW error:', err));
-  });
-}
